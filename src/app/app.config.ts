c7b1c3f7dcbf7befb0d844d9a69522711399f86f
Jhosue Mesias <jhosuemesias@gmail.com>
2024-06-18 08:44:50 -0500

    a

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
 };
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'nosotros', component: NosotrosComponent },
+  // otras rutas aquí
+];
